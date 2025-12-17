6ce1376fd96ad62b33e62a819609937e4f405097
YuktiSoftwares <YuktiSoftwares2021@gmail.com>
2025-12-17 13:49:08 +0530

    INITIAL COMMIT

---
+++ src/main/java/com/yuktisoftware/yukti_website/YuktiWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.yuktisoftware.yukti_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuktiWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuktiWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuktisoftware/yukti_website/YuktiWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuktisoftware.yukti_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuktiWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
